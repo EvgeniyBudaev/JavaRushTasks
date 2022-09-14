fc44468343e73146006c96df3fd20b540d439ff5
Evgeniy Budaev <budaev.e@gmail.com>
2022-09-14 08:34:24 +0300

    2.JavaCore. Особенности вызова конструкторов. Знакомство с нитями ч.2

---
+++ 2.JavaCore/src/com/javarush/task/jdk13/task16/task1624/Solution.java
@@ -0,0 +1,38 @@
+package com.javarush.task.jdk13.task16.task1624;
+
+/* 
+Отсчет на гонках
+*/
+
+public class Solution {
+    public static volatile int numSeconds = 3;
+
+    public static void main(String[] args) throws InterruptedException {
+        RacingClock clock = new RacingClock();
+        //add your code here - добавь код тут
+        Thread.sleep(3500);
+        clock.interrupt();
+    }
+
+    public static class RacingClock extends Thread {
+        public RacingClock() {
+            start();
+        }
+
+        public void run() {
+            //add your code here - добавь код тут
+            try {
+                while (1 <= numSeconds) {
+                    System.out.print(numSeconds + " ");
+                    numSeconds--;
+                    Thread.sleep(1000);
+
+                }
+            } catch (InterruptedException e) {
+                System.out.println("Прервано!");
+                return;
+            }
+            System.out.println("Марш!");
+        }
+    }
+}
